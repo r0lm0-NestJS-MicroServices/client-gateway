2aa76460d4ecd08fc67d1ed79bbb1bb9e0ce16f3
r0lm0 <rolmo92@gmail.com>
2025-09-05 15:33:59 -0600

    Add Orders module and update environment configuration
    - Integrated OrdersModule into AppModule for order management.
    - Added ORDER_MICROSERVICE_HOST and ORDER_MICROSERVICE_PORT to envs.ts for new microservice configuration.
    - Defined ORDER_SERVICE constant in services.ts for service identification.
    - Removed unused imports in update-product.dto.ts for cleaner code.

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, ParseIntPipe, Query } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { ORDER_SERVICE } from 'src/config';
+import { catchError } from 'rxjs';
+
+@Controller('orders')
+export class OrdersController {
+  logger: any;
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy
+  ) { }
+
+  @Post()
+  create(@Query() createOrderDto: CreateOrderDto) {
+    return this.orderClient.send({ cmd: 'create_order' }, createOrderDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      })
+    );
+  }
+
+  @Get()
+  findAll() {
+    return this.orderClient.send({ cmd: 'find_all_orders' }, {}).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      })
+    );
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    return this.orderClient.send({ cmd: 'find_order_by_id' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      })
+    );
+  }
+
+
+}
