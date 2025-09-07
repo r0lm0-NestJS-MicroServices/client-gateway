8ee86803962dc8f1c2ed463890f4e068b8725c86
r0lm0 <rolmo92@gmail.com>
2025-09-06 18:37:15 -0600

    Agregar métodos para buscar y cambiar el estado de órdenes en OrdersController
    - Se añadió el método findAllByStatus para obtener órdenes filtradas por estado, utilizando StatusDto y PaginationDto.
    - Se implementó el método changeStatus para actualizar el estado de una orden específica, manejando errores con RpcException.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,7 +3,8 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
 import { catchError, firstValueFrom } from 'rxjs';
-import { OrderPaginationDto } from './dto';
+import { OrderPaginationDto, StatusDto } from './dto';
+import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -37,6 +38,35 @@ export class OrdersController {
       throw new RpcException(error);
     }
 
+  }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+
+      return this.orderClient.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    try {
+      return this.orderClient.send('changeOrderStatus', { id, status: statusDto.status })
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
 
